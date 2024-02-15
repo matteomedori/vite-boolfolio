7f0699956fe1cf1dea9f6640ef508e5d74ead246
matteomedori <matteo.medori.97@gmail.com>
2024-02-15 16:08:07 +0100

    feat: project card

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,12 +1,22 @@
 <script>
+import ProjectCard from "./ProjectCard.vue";
 export default {
   name: "AppMain",
   props: ["projects"],
+  components: {
+    ProjectCard,
+  },
 };
 </script>
 <template>
   <main>
-    <div class="container py-4">main</div>
+    <div class="container py-4">
+      <h1 class="text-center mb-4">Projects list</h1>
+      <div class="row gy-4">
+        <div class="col col-md-3" v-for="project in projects">
+          <ProjectCard :project="project" />
+        </div>
+      </div>
+    </div>
   </main>
 </template>
-<style></style>
---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  name: "ProjectCard",
+  props: ["project"],
+};
+</script>
+<template>
+  <main>
+    <div class="card" style="width: 18rem">
+      <div class="card-body">
+        <h5 class="card-title">{{ project.title }}</h5>
+        <p class="card-text">
+          {{ project.description.substring(0, 200) }}
+        </p>
+        <a href="#" class="btn btn-primary">Dettagli</a>
+      </div>
+    </div>
+  </main>
+</template>
