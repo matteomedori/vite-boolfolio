ff695925cbe820da8f2c78ffe0ed67af85dee7aa
matteomedori <matteo.medori.97@gmail.com>
2024-02-15 15:37:19 +0100

    feat: header segnaposto

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <script>
+import AppHeader from "./components/AppHeader.vue";
 export default {
   name: "Boolfolio",
+  components: {
+    AppHeader,
+  },
 };
 </script>
 
 <template>
-  <div>prova</div>
+  <AppHeader />
 </template>
 
 <style></style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+  name: "AppHeader",
+};
+</script>
+<template>
+  <header>
+    <div
+      class="container d-flex justify-content-between bg-primary text-white p-3"
+    >
+      <div>logo</div>
+      <nav>
+        <ul class="list-unstyled p-0 m-0 d-flex gap-4">
+          <li>link</li>
+          <li>link</li>
+          <li>link</li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+</template>
+<style></style>
