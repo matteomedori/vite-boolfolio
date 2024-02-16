1c58fef3e12d834d70e873b0496923d7c6e62ce4
matteomedori <matteo.medori.97@gmail.com>
2024-02-16 16:04:31 +0100

    feat: aggiunto appheader

--- src/pages/AppHome.vue
+++ src/pages/AppHome.vue
@@ -1,13 +1,18 @@
 <script>
+import AppHeader from "../components/AppHeader.vue";
 export default {
   name: "AppHome",
+  components: {
+    AppHeader,
+  },
 };
 </script>
 <template>
-  <section>
+  <AppHeader />
+  <main>
     <div class="container py-4">
       <h1 class="text-center mb-4">Homepage</h1>
     </div>
-  </section>
+  </main>
 </template>
 <style scoped></style>
