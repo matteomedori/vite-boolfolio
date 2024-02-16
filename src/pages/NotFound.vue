d5db760297fe01fccde0cccb6f5fa210e0126143
matteomedori <matteo.medori.97@gmail.com>
2024-02-16 16:03:22 +0100

    feat: not found page

---
+++ src/pages/NotFound.vue
@@ -0,0 +1,12 @@
+<script>
+export default {
+  name: "NotFound",
+};
+</script>
+<template>
+  <div class="text-center">
+    <h1>PAGE NOT FOUND</h1>
+    <img src="/assets/spiderman_not_found.png" alt="" />
+  </div>
+</template>
+<style></style>
--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import AppHome from "./pages/AppHome.vue";
 import ProjectList from "./pages/ProjectList.vue";
+import NotFound from "./pages/NotFound.vue";
 const router = createRouter({
   history: createWebHistory(),
   routes: [
@@ -13,6 +14,11 @@ const router = createRouter({
       path: "/projects",
       name: "projects",
       component: ProjectList,
+    },
+    {
+      path: "/:pathMatch(.*)*",
+      name: "not-found",
+      component: NotFound,
     },
   ],
 });
