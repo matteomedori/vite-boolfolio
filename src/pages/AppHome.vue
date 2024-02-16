e715881f3aa409868a7683bcefdbe5a74c3a5006
matteomedori <matteo.medori.97@gmail.com>
2024-02-16 15:34:23 +0100

    feat: h1 centrato

--- src/pages/AppHome.vue
+++ src/pages/AppHome.vue
@@ -5,7 +5,9 @@ export default {
 </script>
 <template>
   <section>
-    <h1 class="mt-5 mb-3">Homepage</h1>
+    <div class="container py-4">
+      <h1 class="text-center mb-4">Homepage</h1>
+    </div>
   </section>
 </template>
 <style scoped></style>
