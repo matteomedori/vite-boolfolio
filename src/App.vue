d5ffb1df236a6f52a7ba2fc7929b70f325260c3c
matteomedori <matteo.medori.97@gmail.com>
2024-02-16 16:04:20 +0100

    feat: rimosso appheader

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,10 @@
 <script>
-import AppHeader from "./components/AppHeader.vue";
-import axios from "axios";
 export default {
   name: "Boolfolio",
-  components: {
-    AppHeader,
-  },
 };
 </script>
 
 <template>
-  <AppHeader />
   <router-view></router-view>
 </template>
 
