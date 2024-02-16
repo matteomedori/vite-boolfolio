3992124966eb7684703baaa84ce620c419048506
matteomedori <matteo.medori.97@gmail.com>
2024-02-16 16:03:35 +0100

    feat: component loading buttons

---
+++ src/components/LoadingButtons.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+  name: "LoadingButtons",
+};
+</script>
+<template>
+  <div class="d-flex justify-content-center gap-2">
+    <button class="btn btn-info" type="button" disabled>
+      <span class="spinner-border spinner-border-sm" aria-hidden="true"></span>
+      <span class="visually-hidden" role="status">Loading...</span>
+    </button>
+    <button class="btn btn-info" type="button" disabled>
+      <span class="spinner-border spinner-border-sm" aria-hidden="true"></span>
+      <span role="status">Loading...</span>
+    </button>
+  </div>
+</template>
